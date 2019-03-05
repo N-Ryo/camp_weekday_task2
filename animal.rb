e3528721ada9d5eb6422dfbede83e95252846946
N-Ryo <ryo.joy.joy@gmail.com>
2019-03-05 16:31:51 +0900

    Add about issue of exercise02-5.rbが期待通りの出力をすること。

--- animal.rb
+++ animal.rb
@@ -1,3 +1,5 @@
+# 課題2-5 Dog,Catクラスの追加と実装
+
 class Animal
   attr_reader :name, :voice
 
@@ -8,5 +10,25 @@ class Animal
 
   def cry
     "#{self.name}: #{voice}!"
+  end
+end
+
+#2-5 Dogクラスの定義（Animalクラスの継承）
+class Dog < Animal
+  def initialize(name)
+    # 親クラスのnameの定義を継承する
+    super name
+    # cryメソッドのvioceの呼び出す値を変更する
+    @voice = "ワンワン"
+  end
+end
+
+#2-5 Catクラスの定義（Animalクラスの継承）
+class Cat < Animal
+  def initialize(name)
+    # 親クラスのnameの定義を継承する
+    super name
+    # cryメソッドのvioceの呼び出す値を変更する
+    @voice = "ニャー"
   end
 end
